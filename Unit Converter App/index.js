b2be8681928029929b2043fa70629c4d89878c40
ll3t-abhishek <129820001+ll3t-abhishek@users.noreply.github.com>
2026-09-13 15:54:44 +0530

    completed unit conversion

--- Unit Converter App/index.js
+++ Unit Converter App/index.js
@@ -1,27 +1,53 @@
 const inputEl = document.getElementById("input-el");
 const buttonEl = document.getElementById("button-el");
 let meterToFeet = document.getElementById("meterToFeet");
+let feetToMeter = document.getElementById("feetToMeter");
 let inputValue = document.querySelectorAll(".input-value");
 
+let litresToGallon = document.getElementById("litresToGallon");
+let gallonToLitres = document.getElementById("gallonToLitres");
+let kgToPounds = document.getElementById("kgToPounds");
+let poundToKg = document.getElementById("poundToKg");
 
-buttonEl.addEventListener("click", convert)
+inputEl.addEventListener("wheel", (e) => e.target.blur());
+buttonEl.addEventListener("click", convert);
 
 function convert(){
-inputValue.textContent = " " + inputEl.value;
-meterToFeet.textContent = lenConv(Number(inputEl.value)).toFixed(3);
-console.log(lenConv(inputEl.value));
-console.log(inputEl.value);
+const value = Number(inputEl.value);
 
+inputValue.forEach(item => {
+    item.textContent = value;
+  });
+
+  meterToFeet.textContent = lenConv(value).toFixed(3);
+  feetToMeter.textContent = lenConvInv(value).toFixed(3);
+  litresToGallon.textContent = volConv(value).toFixed(3);
+  gallonToLitres.textContent = volConvInv(value).toFixed(3);
+  kgToPounds.textContent = masConv(value).toFixed(3);
+  poundToKg.textContent = masConvInv(value).toFixed(3);
 }
 
 
 function lenConv(inputNum) {
   return inputNum * 3.28084
 }
+function lenConvInv(inputNum) {
+  return inputNum / 3.28084
+}
+
+
+
 function volConv(inputNum) {
   return inputNum * 0.264172
 }
+function volConvInv(inputNum) {
+  return inputNum / 0.264172
+}
+
 function masConv(inputNum) {
   return inputNum * 2.20462
+}
+function masConvInv(inputNum) {
+  return inputNum / 2.20462
 }
 
