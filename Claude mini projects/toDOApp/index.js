5e68f237fc73ed71a6dd2dfd29150eb560287431
ll3t-abhishek <129820001+ll3t-abhishek@users.noreply.github.com>
2026-09-16 09:38:21 +0530

    update

--- Claude mini projects/toDOApp/index.js
+++ Claude mini projects/toDOApp/index.js
@@ -2,21 +2,19 @@ let inputEl = document.getElementById("input-el");
 let addList = document.querySelector(".add-list");
 let storedTasks = document.querySelector(".stored-tasks")
 
-// addList.addEventListener("click", function() {
-//     let userInput = inputEl.value;
-//     console.log(userInput);
 
-// })
 
-const toDOTask = {
-    taskSelect: "ssssssssss",
-    taskData: "ssssssssssssss"
-}
 
-storedTasks.value = toDOTask;
+
 
 function f1(){
     let userInput = inputEl.value;
     console.log(userInput);
+    let newElement = document.createElement("p");
+
+newElement.textContent = inputEl.value;
+newElement.className = "cards";
+
+storedTasks.appendChild(newElement);
 }
-x
+
